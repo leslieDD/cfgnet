e02de962749f8e9868b45cf57e5b2cae36987aec
DianDian <leslie@dd.com>
2023-09-17 00:36:57 +0800

    fix

--- cfgnet.py
+++ cfgnet.py
@@ -42,7 +42,7 @@ def example():
     print(colored('# 给文件hosts中的所有主机 (网关所在网络接口) 配置新地址', 'blue'))
     print(colored('./cfgnet.py -p hosts -n 10.30.200.0/24 --add', 'yellow'))
     print(colored('# 给文件hosts中的所有主机 (网关所在网络接口) 配置新地址，没有设定网关，跳过DNS，指定分配起始地址', 'blue'))
-    print(colored('./cfgnet.py -p hosts -n 10.30.220.0/24 -s 10.30.220.100 -d '' --add', 'yellow'))
+    print(colored('./cfgnet.py -p hosts -n 10.30.220.0/24 -s 10.30.220.100 -d "" --add', 'yellow'))
 
     print(colored('# 尝试识别文件中的IP地址,排序结果并输出', 'blue'))
     print(colored('./cfgnet.py -S addr_file', 'yellow'))
